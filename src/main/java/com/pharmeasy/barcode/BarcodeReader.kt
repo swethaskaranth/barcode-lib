e066ae5191236c7ea8e429fc100c11a6ea46e5bb
Akshay Kumar H <akshay.h@pharmeasy.in>
2019-11-13 11:42:15 +0530

    refactor: removed the application class and created a singleton implementation for the same.

--- src/main/java/com/pharmeasy/barcode/BarcodeReader.kt
+++ src/main/java/com/pharmeasy/barcode/BarcodeReader.kt
@@ -1,8 +1,7 @@
 package com.pharmeasy.barcode
 
-import android.app.Activity
-import android.app.Application
 import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
@@ -15,19 +14,20 @@ import com.pharmeasy.barcode.interfaces.Scanner
 import com.pharmeasy.barcode.scanners.NewlandScanner
 import com.pharmeasy.barcode.scanners.ZebraScanner
 
-class BarcodeReader :Application() {
-
-    companion object {
+class BarcodeReader private constructor(context: Context){
+    val mContext=context
+    companion object:SingletonHolder<BarcodeReader,Context>(::BarcodeReader) {
         val barcode = MutableLiveData<String>()
     }
 
-    lateinit var scanner: Scanner
-    lateinit var broadcastReceiver: BroadcastReceiver
+
+
+     var scanner: Scanner
+     var broadcastReceiver: BroadcastReceiver
     var intentFilter: IntentFilter= IntentFilter()
     lateinit var profileBroadCast:ProfileBroadCast
 
-    override fun onCreate() {
-        super.onCreate()
+    init {
 
 
         if(Build.MANUFACTURER.contains("Zebra Technologies") || Build.MANUFACTURER.contains("Motorola Solutions")){
@@ -39,75 +39,39 @@ class BarcodeReader :Application() {
             broadcastReceiver=ZebraBroadcast()
             profileBroadCast= ProfileBroadCast()
             intentFilter.addCategory(Intent.CATEGORY_DEFAULT)
-            intentFilter.addAction(resources.getString(R.string.activity_intent_filter_action))
-       }else{
+            intentFilter.addAction(context.getString(R.string.activity_intent_filter_action))
+        }else{
             scanner = NewlandScanner()
             broadcastReceiver=NewlandBroadCast()
-            intentFilter.addAction(resources.getString(R.string.newland_scanner_result))
+            intentFilter.addAction(context.getString(R.string.newland_scanner_result))
 
         }
+    }
 
+    fun registerBroadcast(context: Context){
+        context.registerReceiver(broadcastReceiver,intentFilter)
+        context.registerReceiver(profileBroadCast,
+            IntentFilter(context.getString(R.string.datawedge_intent_key_result_action))
+        )
+    }
 
-        registerActivityLifecycleCallbacks(object: ActivityLifecycleCallbacks{
-
-            override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
-            }
-
-
-            override fun onActivityStarted(activity: Activity?) {
-            }
-
-            override fun onActivityResumed(activity: Activity?) {
-                registerReceiver(broadcastReceiver,intentFilter)
-                registerReceiver(profileBroadCast,
-                    IntentFilter(getString(R.string.datawedge_intent_key_result_action))
-                )
-
-            }
-
-            override fun onActivityPaused(activity: Activity?) {
-            }
-
-            override fun onActivityStopped(activity: Activity?) {
-                unregisterReceiver(broadcastReceiver)
-                unregisterReceiver(profileBroadCast)
-
-            }
-
-            override fun onActivityDestroyed(activity: Activity?) {
-            }
-
-
-
-            override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
-            }
-        })
-
+    fun unregisterBroadcast(context: Context){
+        context.unregisterReceiver(broadcastReceiver)
+        context.unregisterReceiver(profileBroadCast)
     }
 
 
+
      fun enableScanner(){
-        this.scanner.enableScanner(this)
+        this.scanner.enableScanner(mContext)
      }
 
     fun disableScanner(){
-        this.scanner.disableScanner(this)
+        this.scanner.disableScanner(mContext)
     }
 
 
     private fun createBarCodeProfileForZebraDevice(){
-
-
-        // Configuration for how to output the barcode
-       /* val outputBundleParams = Bundle()
-        outputBundleParams.putString(KEY_INTENT_OUTPUT_ENABLED, VALUE_INTENT_OUTPUT_ENABLED_TRUE) // enable this profile
-        outputBundleParams.putString(KEY_INTENT_ACTION,resources.getString(R.string.activity_intent_filter_action)) // set the action to which the broadcastlistener listens to get barcode
-        outputBundleParams.putString(KEY_INTENT_DELIVERY, VALUE_INTENT_DELIVERY_BROADCAST) // deliver via broadcast
-
-        val outputBundle = Bundle()
-        outputBundle.putString(KEY_PLUGIN_NAME, VALUE_PLUGIN_NAME_INTENT)
-        outputBundle.putString(KEY_RESET_CONFIG, VALUE_RESET_CONFIG_TRUE)
-        outputBundleParams.putBundle(KEY_PARAM_LIST,outputBundleParams)*/
 
         // Configuration for barcode reader
         val barcodeBundle = Bundle()
@@ -118,7 +82,7 @@ class BarcodeReader :Application() {
         barcodeBundle.putBundle(KEY_PARAM_LIST,barcodeBundleParams)
 
         val appConfig = Bundle()
-        appConfig.putString(KEY_PACKAGE_NAME,packageName)
+        appConfig.putString(KEY_PACKAGE_NAME,mContext.packageName)
         appConfig.putStringArray(KEY_ACTIVITY_LIST, arrayOf("*"))
 
         val mainBundle = Bundle()
@@ -133,10 +97,10 @@ class BarcodeReader :Application() {
 
 
         val intent = Intent()
-        intent.setAction(getString(R.string.datawedge_api_intent_action))
-        intent.putExtra(getString(R.string.datawedge_api_intent_extra_config),mainBundle)
+        intent.setAction(mContext.getString(R.string.datawedge_api_intent_action))
+        intent.putExtra(mContext.getString(R.string.datawedge_api_intent_extra_config),mainBundle)
         intent.putExtra(KEY_SEND_RESULT,VALUE_SEND_RESULT_TRUE)
-        sendBroadcast(intent)
+        mContext.sendBroadcast(intent)
 
     }
 
@@ -152,7 +116,7 @@ class BarcodeReader :Application() {
         // Enable the intent output
         outputBundleParams.putString(KEY_INTENT_OUTPUT_ENABLED, VALUE_INTENT_OUTPUT_ENABLED_TRUE)
         // Set the action string to which the broadcast listener listens to get barcode
-        outputBundleParams.putString(KEY_INTENT_ACTION,getString(R.string.activity_intent_filter_action).toString())
+        outputBundleParams.putString(KEY_INTENT_ACTION,mContext.getString(R.string.activity_intent_filter_action).toString())
         // Deliver via broadcast the barcode which was read.
         outputBundleParams.putString(KEY_INTENT_DELIVERY, VALUE_INTENT_DELIVERY_BROADCAST)
 
@@ -171,12 +135,10 @@ class BarcodeReader :Application() {
         mainBundle.putBundle(KEY_PLUGIN_CONFIG,outputBundle)
 
         val intent = Intent()
-        intent.setAction(getString(R.string.datawedge_api_intent_action))
-        intent.putExtra(getString(R.string.datawedge_api_intent_extra_config),mainBundle)
+        intent.setAction(mContext.getString(R.string.datawedge_api_intent_action))
+        intent.putExtra(mContext.getString(R.string.datawedge_api_intent_extra_config),mainBundle)
         intent.putExtra(KEY_SEND_RESULT,VALUE_SEND_RESULT_TRUE)
-        sendBroadcast(intent)
-
-
+        mContext.sendBroadcast(intent)
 
     }
 
@@ -185,11 +147,11 @@ class BarcodeReader :Application() {
      */
     fun createProfileForZebraDevice(){
        val intent = Intent()
-       intent.setAction(getString(R.string.datawedge_api_intent_action))
-       intent.putExtra(getString(R.string.datawedge_intent_key_create_profile), VALUE_PROFILE_NAME)
+       intent.setAction(mContext.getString(R.string.datawedge_api_intent_action))
+       intent.putExtra(mContext.getString(R.string.datawedge_intent_key_create_profile), VALUE_PROFILE_NAME)
        intent.putExtra(KEY_SEND_RESULT, VALUE_SEND_RESULT_TRUE)
        intent.putExtra(KEY_COMMAND_IDENTIFIER, VALUE_COMMAND_IDENTIFIER)
-       this.sendBroadcast(intent)
+       mContext.sendBroadcast(intent)
    }
 
     /**
@@ -198,11 +160,11 @@ class BarcodeReader :Application() {
 
     fun switchToProfile(profileName:String){
         val intent = Intent()
-        intent.setAction(getString(R.string.datawedge_intent_key_switch_profile))
-        intent.putExtra(getString(R.string.datawedge_intent_key_extra_profilename), profileName)
+        intent.setAction(mContext.getString(R.string.datawedge_intent_key_switch_profile))
+        intent.putExtra(mContext.getString(R.string.datawedge_intent_key_extra_profilename), profileName)
         intent.putExtra(KEY_SEND_RESULT,VALUE_SEND_RESULT_TRUE)
         intent.putExtra(KEY_COMMAND_IDENTIFIER, VALUE_COMMAND_IDENTIFIER)
-        this.sendBroadcast(intent)
+        mContext.sendBroadcast(intent)
     }
 
 
