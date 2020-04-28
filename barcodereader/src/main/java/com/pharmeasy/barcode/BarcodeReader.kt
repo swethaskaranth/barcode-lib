06cf0ea4a2a84bdec8c5ae248155a5749d75cc50
swetha <swetha.s@pharmeasy.in>
2020-04-28 19:12:43 +0530

    Removed mode assignment

--- barcodereader/src/main/java/com/pharmeasy/barcode/BarcodeReader.kt
+++ barcodereader/src/main/java/com/pharmeasy/barcode/BarcodeReader.kt
@@ -149,10 +149,6 @@ class BarcodeReader private constructor(context: Context) : DecoratedBarcodeView
     private fun showAlertDialog(activity: Activity, intent: Intent, i: Int, editText: EditText?, listener: ModeSelectedListener) {
         val items = ScannerType.toStringList().toTypedArray()
 
-        mode = items[2]
-
-        Toast.makeText(activity,"Index - "+items.indexOf(mode)+", Item - "+mode,Toast.LENGTH_SHORT).show()
-
         val builder = AlertDialog.Builder(activity)//ERROR ShowDialog cannot be resolved to a type
         builder.setTitle("Choose Scanner")
         builder.setSingleChoiceItems(items, if(mode != null) items.indexOf(mode) else 2) { dialog, item ->
